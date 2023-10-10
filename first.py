2363b5b5bdaf09f7686be7466ba8064f5a2ff744
umesshreddy366 <umesshreddy01@gmail.com>
2023-10-10 11:08:24 +0530

    push

---
+++ first.py
@@ -0,0 +1,9 @@
+import math
+a = math.factorial(3)
+print("factorial of 3 = ",a)
+# printing 1 to 10
+print("prinitng first  10 natural numbers")   
+n = 10
+for i in range(1,n+1):
+    print(i)
+# 
